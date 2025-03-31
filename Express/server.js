a312a183a5bf0e4d1db949a8b388bbffc7263822
Jonas Dally <jonas.dally@hotmail.de>
2025-03-31 23:33:16 +0200

    changes express server

--- Express/server.js
+++ Express/server.js
@@ -2,19 +2,27 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const next = require('next');
+const cors = require('cors');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.prepare().then(() => {
     const server = express();
     const httpServer = http.createServer(server);
-    const io = new Server(httpServer);
+    const io = new Server(httpServer, {
+        cors: {
+            origin: "*", // Testweise, später spezifische URL erlauben
+            methods: ["GET", "POST"]
+        },
+        path: '/socket.io', // Wichtig, um Route korrekt zu definieren
+    });
 
-  
+    server.use(cors()); // Cors für Express erlauben
+
     io.on('connection', (socket) => {
         console.log(`🔌 User connected: ${socket.id}`);
 
@@ -28,7 +36,6 @@ app.prepare().then(() => {
         });
     });
 
- 
     server.all('*', (req, res) => handle(req, res));
 
     httpServer.listen(PORT, () => {
